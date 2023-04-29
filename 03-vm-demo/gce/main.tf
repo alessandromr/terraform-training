87beac2395f28fbed6dfc582d0df8217fa2861bb
Alessandro Marino <alessandromarinoac@gmail.com>
2023-04-29 18:36:36 +0200

    Placeholder

---
+++ 03-vm-demo/gce/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("<NAME>.json")
+
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
